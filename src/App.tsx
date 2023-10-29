97e3f24ba4792b74e7ef7c08afb921a8bb55ca27
Mateus Garcia <mpgx5.c@gmail.com>
2023-10-28 23:10:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const App = () => {
     Notification.requestPermission();
 
     const eventSource = new EventSource(
-      `https://${subdomain}.uzzepedidos.online//sse`,
+      `https://${subdomain}.uzzepedidos.online/sse`,
       {
         withCredentials: false,
       }
